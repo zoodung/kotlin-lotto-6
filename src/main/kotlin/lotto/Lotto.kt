44b00086ffbd9d9884dd15d7c560f790e0af75d0
zoodung <zoodung@naver.com>
2023-11-04 03:03:14 +0900

    feat: 로또 발행 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,21 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == LOTTO_NUMBER_LENGTH)
+        require(numbers.toSet().size == LOTTO_NUMBER_LENGTH)
+        require(numbers.all { it in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER })
     }
 
-    // TODO: 추가 기능 구현
+    fun pickLottoNumbers(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_LENGTH)
+    }
+
+    companion object {
+        private const val MIN_LOTTO_NUMBER = 1
+        private const val MAX_LOTTO_NUMBER = 45
+        private const val LOTTO_NUMBER_LENGTH = 6
+    }
 }
