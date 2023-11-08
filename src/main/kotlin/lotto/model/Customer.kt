d1c829514dd868b50fdec0fa2cf0c59f63e8e9b9
zoodung <zoodung@naver.com>
2023-11-08 16:32:47 +0900

    refactor(Customer): 사용하지 않는 import문 삭제

--- src/main/kotlin/lotto/model/Customer.kt
+++ src/main/kotlin/lotto/model/Customer.kt
@@ -1,9 +1,6 @@
 package lotto.model
 
-import lotto.utils.Constants
 import lotto.utils.Constants.INITIALIZE_NUMBER
-import lotto.utils.Constants.LOTTO_PURCHASE_UNIT
-
 
 class Customer(
     private var pay: Int = INITIALIZE_NUMBER,
